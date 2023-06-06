08aa9ef427cd1232caad324418d0b04c9b3fa2dd
Роман Носов <romannosov_2001@mail.ru>
2023-06-06 20:42:04 +0300

    Added dependencies. Getting settings from file and choosing nickname is implemented.

--- Client/src/main/java/org/example/Client.java
+++ Client/src/main/java/org/example/Client.java
@@ -1,7 +1,58 @@
 package org.example;
 
-public class Client {
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Client implements TCPConnectionListener {
+    public static Scanner scanner = new Scanner(System.in);
+    public static JSONParser parser = new JSONParser();
+//    private static TCPConnection connection;
+    public static String serverIP;
+    public static long serverPort;
+    private static final String PATH_TO_SETTINGS = Paths.get("").toAbsolutePath().toString() +
+            "/Client/src/main/java/org/example/settings.json";
+    public static String nikName;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Object obj = parser.parse(new FileReader(PATH_TO_SETTINGS));
+            JSONObject jsonObject = (JSONObject) obj;
+            serverIP = (String) jsonObject.get("serverIP");
+            serverPort = (Long) jsonObject.get("serverPort");
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print("Введите отображаемое в чате имя: ");
+        nikName = scanner.nextLine();
+
+
+    }
+
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+
     }
 }
